c29b9d1d80781d305360a8c47fd84d096e0ab708
pavansailyros <pavansai@lyrostech.com>
2024-03-05 19:37:22 +0530

    css added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import Cakes from './Cakes';
+import Header from './Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <Header />
     <Cakes />
+   
   </React.StrictMode>
 );
 
