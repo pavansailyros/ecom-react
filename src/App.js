f30bb7b69fd1cd594c79d9ddfba448c08c3118ae
pavansailyros <pavansai@lyrostech.com>
2024-03-05 15:23:56 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,60 +1,25 @@
-import React from 'react'
+import logo from './logo.svg';
+import './App.css';
 
-export default function Cakes() {
-    let Products=[
-        {
-            name:" Chocolate Cake",
-            description: "Melting Chocolate Cake",
-            price:599,
-            src:'/Image/cake1.jpg',
-        },
-        {
-            name:"vanilla cake",
-            description: "Fresh vanilla cake ",
-            price:499,
-           src:'./Images/cake2.jpg',
-        },
-        {
-            name:"Rainbow cake",
-            description: "fresh Rain bow cake",
-            price:499,
-            src:'./Image/cake1.jpg'
-        },
-        {
-            name:"White Forest cake",
-            description: "Eggless white forest cake",
-            price:599,
-            src:'./Images/cake4.jpg'
-        },
-        {
-            name:"Pink cake",
-            description: "80 Rose Pink Strawberry",
-            price:599,
-            src:'./Images/cake5.jpg'
-        },
-        {
-            name:"Black Forest",
-            description: " Fresh Black Forest",
-            price:699,
-            src:'./Images/cake6.jpg'
-        }
-    ];
-
-
+function App() {
   return (
-    <div className='Main'> 
-    
-    {Products.map((value,i)=>(      
-    <div className='Cakes'>
-    <div> <img width ="180px" height="170px" src={value.src}  alt='images' ></img></div>
-     <div><b>{value.name}</b></div>
-     <div><i>{value.description}</i></div>
-     <div>{value.price}.Rs</div>
-    <div><button>Add</button></div>
-        </div>
-       
-        ))}
-  
-</div>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
