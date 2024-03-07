cb37b64ecbfee4d49bdbdb7a525ee3fe5ebddd2a
pavansailyros <pavansai@lyrostech.com>
2024-03-07 20:07:10 +0530

    logo added

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <div className='header'>
-     Delicious cakes 
+     Delicious cakes <span><img src='./pictures/logo.png'></img></span>
     </div>
   )
 }
