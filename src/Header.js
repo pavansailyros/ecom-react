2c0a441a2bcf8aa45be8dcc33afaa1627f59de35
pavansailyros <pavansai@lyrostech.com>
2024-03-07 20:13:03 +0530

    changes

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <div className='header'>
-     Delicious cakes <span><img src='./pictures/logo.png' alt='logo'></img></span>
+     Delicious cakes <span><img src='./pictures/logo.png' width={"70px"} height={"70px"} alt='logo'></img></span>
     </div>
   )
 }
