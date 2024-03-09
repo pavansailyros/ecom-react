9abd03ca51ccc55b1d12cf635918767fe00f0175
pavansailyros <pavansai@lyrostech.com>
2024-03-09 10:27:46 +0530

    added footer

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <p>&copy;  100% On Time Delivery  |
+           
+       
+        
+                
+           | 100% Fresh & Hygienic
+        
+                |FSSAI Certified.</p>
+    <p>Contact: <a href="mailto:Delicious.com">info@DeliciousCake.com</a></p>
+    <p>Follow us on: <a href="https://lyrostech.com/" target="_blank">Our website</a> 
+        </p>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 
 import Cakes from './Cakes';
 import Header from './Header';
+import Footer from './Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
      <Header />
     <Cakes />
+    <Footer />
    
   </React.StrictMode>
 );
