46f63c936689798300065afdf77f9d34e9fe510e
pavansailyros <pavansai@lyrostech.com>
2024-03-12 15:15:59 +0530

    add

--- src/Cakes.js
+++ src/Cakes.js
@@ -1,60 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './index.css';
 
 export default function Cakes() {
-    let Products=[
-        {
-            name:" Chocolate Cake",
-            description: "Melting Chocolate Cake",
-            price:599,
-            src:'./pictures/img1.jpg',
-        },
-        {
-            name:"vanilla cake",
-            description: "Fresh vanilla cake ",
-            price:499,
-           src:'./pictures/img2.jpg',
-        },
-        {
-            name:"Rainbow cake",
-            description: "fresh Rain bow cake",
-            price:499,
-            src:'./pictures/img3.jpg'
-        },
-        {
-            name:"White Forest cake",
-            description: "Eggless white forest cake",
-            price:599,
-            src:'./pictures/img4.jpg'
-        },
-        {
-            name:"Pink cake",
-            description: "80 Rose Pink Strawberry",
-            price:599,
-            src:'./pictures/img5.jpg'
-        },
-        {
-            name:"Black Forest",
-            description: " Fresh Black Forest",
-            price:699,
-            src:'./pictures/img6.jpg'
-        }
-    ];
+  const [cartItems, setCartItems] = useState([]);
 
+  const Products = [
+    {
+      name: "Chocolate Cake",
+      description: "Melting Chocolate Cake",
+      price: 599,
+      src: './pictures/img1.jpg',
+    },
+    {
+      name: "Vanilla Cake",
+      description: "Fresh vanilla cake ",
+      price: 499,
+      src: './pictures/img2.jpg',
+    },
+    {
+      name: "Rainbow cake",
+      description: "Fresh Rain bow cake",
+      price: 499,
+      src: './pictures/img3.jpg',
+    },
+    {
+      name: "White Forest cake",
+      description: "Eggless white forest cake",
+      price: 599,
+      src: './pictures/img4.jpg',
+    },
+    {
+      name: "Pink cake",
+      description: "80 Rose Pink Strawberry",
+      price: 599,
+      src: './pictures/img5.jpg',
+    },
+    {
+      name: "Black Forest",
+      description: "Fresh Black Forest",
+      price: 699,
+      src: './pictures/img6.jpg',
+    },
+  ];
+
+  const addToCart = (product) => {
+    setCartItems([...cartItems, product]);
+  };
 
   return (
-    <div className='Main'> 
-    
-    {Products.map((value,i)=>(      
-    <div className='Cakes'>
-    <div> <img width ="180px" height="170px" src={value.src}  alt='images' ></img></div>
-     <div><b>{value.name}</b></div>
-     <div><i>{value.description}</i></div>
-     <div>{value.price}.Rs</div>
-    <div><button>Add</button></div>
+    <div className='Main'>
+      {Products.map((product, index) => (
+        <div key={index} className='Cakes'>
+          <div><img width="180px" height="170px" src={product.src} alt='images' /></div>
+          <div><b>{product.name}</b></div>
+          <div><i>{product.description}</i></div>
+          <div>{product.price}.Rs</div>
+          <div><button onClick={() => addToCart(product)}>Add</button></div>
         </div>
-       
-        ))}
-  
-</div>
-  )
+      ))}
+    </div>
+  );
 }
